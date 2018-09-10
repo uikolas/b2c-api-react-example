25ba6a5dc54f900b7bdb19daf9993e868a8411a7
yevgenyermakov <een3838@gmail.com>
2018-09-10 18:40:37 +0300

    add

--- src/shared/services/Pages/Home.ts
+++ src/shared/services/Pages/Home.ts
@@ -6,20 +6,6 @@ const api = new ApiClient();
 export class PagesHomeService {
   public static async getData(ACTION_TYPE: string, dispatch: Function): Promise<any> {
     try {
-      const query = {
-        "data": {
-          "type": "customers",
-          "attributes": {
-            "salutation": "Mr",
-            "firstName": "First",
-            "lastName": "test",
-            "email": "zfort@test.com",
-            "password": "zzzzzzzz",
-            "passwordConfirmation": "zzzzzzzz",
-            "acceptedTerms": true
-          }
-        }
-      };
       // const result: any = await api.post(`${config.API_URL}customers`, query )
       // console.info(result);
       //
---
+++ src/shared/services/Pages/Login.ts
@@ -0,0 +1,74 @@
+import {config} from '../../config';
+import ApiClient from '../api/ApiClient';
+
+const api = new ApiClient();
+
+export class PagesLoginService {
+  public static async register(ACTION_TYPE: string, dispatch: Function): Promise<any> {
+    try {
+      const body = {
+        "data": {
+          "type": "customers",
+          "attributes": {
+            "salutation": "Mr",
+            "firstName": "First",
+            "lastName": "test",
+            "email": "zfort@test.com",
+            "password": "zzzzzzzz",
+            "passwordConfirmation": "zzzzzzzz",
+            "acceptedTerms": true
+          }
+        }
+      };
+      // const result: any = await api.post(`${config.API_URL}customers`, body )
+      // console.info(result);
+      //
+      // dispatch({
+      //   type: ACTION_TYPE + '_FULFILLED',
+      //   payload: result,
+      // });
+      // return result;
+      return null;
+    } catch (error) {
+      console.error('register', error);
+      dispatch({
+        type: ACTION_TYPE + '_REJECTED',
+        error,
+      });
+      return null;
+    }
+  }
+
+  public static async loginRequest(ACTION_TYPE: string, dispatch: Function): Promise<any> {
+    try {
+      const body = {
+        "data": {
+          "type": "string",
+          "attributes": {
+            "username": "string",
+            "password": "string"
+          },
+          "links": {
+            "self": "string"
+          }
+        }
+      };
+
+      const result: any = await api.post(`${config.API_URL}access-tokens`, body );
+      console.info(result);
+
+      dispatch({
+        type: ACTION_TYPE + '_FULFILLED',
+        payload: result,
+      });
+      return result;
+    } catch (error) {
+      console.error('login', error);
+      dispatch({
+        type: ACTION_TYPE + '_REJECTED',
+        error,
+      });
+      return null;
+    }
+  }
+}
