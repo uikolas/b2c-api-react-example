72362fa0284d90a6766d7498230cc10accf933dd
PavelKovalchuk <kovpavanat@gmail.com>
2018-09-13 18:08:23 +0300

    Preloader - testing

---
+++ test/unit/components/Common/Preloader/Preloader.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { HTMLAttributes, shallow, ShallowWrapper, mount } from "enzyme";
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+import {
+  PreloaderBase
+} from '../../../../../src/shared/components/Common/Preloader';
+import {styles} from '../../../../../src/shared/components/Common/Preloader/styles';
+
+
+// Required props
+
+const dispatch = () => null;
+
+const getShallowedComponent = () => (
+  shallow(
+    <PreloaderBase
+      classes = {styles}
+    />
+  )
+);
+
+describe('components->Common->Preloader', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper =  getShallowedComponent();
+  });
+
+  it("renders the component", () => {
+    expect(wrapper.find(LinearProgress)).toHaveLength(2);
+  });
+
+});
