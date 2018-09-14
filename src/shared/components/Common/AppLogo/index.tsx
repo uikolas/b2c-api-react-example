e2c95829e8e006a2b8701be06c6d5c71d093a218
PavelKovalchuk <kovpavanat@gmail.com>
2018-09-14 17:41:28 +0300

    Added logo svg

--- src/shared/components/Common/AppLogo/index.tsx
+++ src/shared/components/Common/AppLogo/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
+import { NavLink } from 'react-router-dom';
 
+import {logoIcon} from '../../../../assets/icons/logo';
 import {styles} from './styles';
 
 interface AppLogoProps extends WithStyles<typeof styles> {
@@ -12,7 +14,7 @@ export const AppLogoBase: React.SFC<AppLogoProps> = (props) => {
   const { classes } = props;
   return (
     <Typography variant="title" color="inherit" noWrap className={classes.logo}>
-      Spryker Logo
+      <NavLink to="/">{logoIcon}</NavLink>
     </Typography>
   );
 };
