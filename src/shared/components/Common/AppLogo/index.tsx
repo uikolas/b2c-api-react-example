5900cd3a65f8f7a86d0e9d509dcaefdb92ded3bf
PavelKovalchuk <kovpavanat@gmail.com>
2018-09-14 18:54:14 +0300

    Added logo svg - refactored

--- src/shared/components/Common/AppLogo/index.tsx
+++ src/shared/components/Common/AppLogo/index.tsx
@@ -3,7 +3,7 @@ import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
 
-import {logoIcon} from '../../../../assets/icons/logo';
+import {IconLogo} from '../../../assets/icons/IconLogo';
 import {styles} from './styles';
 
 interface AppLogoProps extends WithStyles<typeof styles> {
@@ -13,9 +13,7 @@ interface AppLogoProps extends WithStyles<typeof styles> {
 export const AppLogoBase: React.SFC<AppLogoProps> = (props) => {
   const { classes } = props;
   return (
-    <Typography variant="title" color="inherit" noWrap className={classes.logo}>
-      <NavLink to="/">{logoIcon}</NavLink>
-    </Typography>
+    <NavLink className={classes.logo} id="AppLogo" to="/"><IconLogo/></NavLink>
   );
 };
 
