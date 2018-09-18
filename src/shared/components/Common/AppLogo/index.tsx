0a74243434d24f50a0ed4137313f045797fee93d
PavelKovalchuk <kovpavanat@gmail.com>
2018-09-18 10:44:04 +0300

    AppLogo modified

--- src/shared/components/Common/AppLogo/index.tsx
+++ src/shared/components/Common/AppLogo/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
 import { NavLink } from 'react-router-dom';
 
 import {IconLogo} from '../../../assets/icons/IconLogo';
