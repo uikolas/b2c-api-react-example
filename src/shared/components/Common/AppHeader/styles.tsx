3ec75e5654d2e70b65f9867935676045cd90c609
PavelKovalchuk <kovpavanat@gmail.com>
2018-10-04 10:40:18 +0300

    fixed header styles

--- src/shared/components/Common/AppHeader/styles.tsx
+++ src/shared/components/Common/AppHeader/styles.tsx
@@ -3,6 +3,7 @@ import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
   appBar: {
+    minHeight: "65px",
   },
   button: {
     marginLeft: theme.spacing.unit,
