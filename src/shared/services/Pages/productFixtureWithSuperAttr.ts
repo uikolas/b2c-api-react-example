f6320170a76fca5b7ea8ebc90d9cebdb8d73d80d
PavelKovalchuk <kovpavanat@gmail.com>
2018-10-09 11:15:27 +0300

    Revert "added request error in product"
    This reverts commit b06c59ffb9dae0e97ff7fc9504a8feb0643d32ca.

--- src/shared/services/Pages/productFixtureWithSuperAttr.ts
+++ src/shared/services/Pages/productFixtureWithSuperAttr.ts
@@ -2173,5 +2173,5 @@ export const fixtureOneProduct: any = {
 };
 
 export const fixtureError: any = {
-  "errors":[{"code":"301","status":404,"detail":"Abstract product is not found."}]
+  data: {"errors":[{"code":"301","status":404,"detail":"Abstract product is not found."}]}
 }
