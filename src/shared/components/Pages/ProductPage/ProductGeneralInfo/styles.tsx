e6bf9322a2b9b2cb14868a2126ec00601a65d87c
PavelKovalchuk <kovpavanat@gmail.com>
2018-10-24 13:38:19 +0300

    AppPageTitle

--- src/shared/components/Pages/ProductPage/ProductGeneralInfo/styles.tsx
+++ src/shared/components/Pages/ProductPage/ProductGeneralInfo/styles.tsx
@@ -3,7 +3,7 @@ import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
   root: {
-    borderBottom: `1px solid ${theme.palette.grey.A100}`,
+    borderBottom: `1px solid ${theme.appColors.blockDivider}`,
     paddingBottom: theme.spacing.unit * 2,
     paddingLeft: theme.spacing.unit,
   },
