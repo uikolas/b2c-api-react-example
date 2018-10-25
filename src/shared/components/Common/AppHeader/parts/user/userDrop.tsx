ad85d714db4e6e239b5d22da33c1fb1bf347d21d
UFleming <fleming@bluefountainmedia.com>
2018-10-25 12:12:50 +0300

    Working on user drop

--- src/shared/components/Common/AppHeader/parts/user/userDrop.tsx
+++ src/shared/components/Common/AppHeader/parts/user/userDrop.tsx
@@ -4,7 +4,6 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
 
 import {
-  pathCustomerPage,
   pathCustomerProfilePage,
   pathHomePage,
   pathLoginPage,
