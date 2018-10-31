d0d0e88dca6d3ee290925363b78cfefd5e0036f2
UFleming <fleming@bluefountainmedia.com>
2018-10-31 13:35:40 +0200

    Added sticky functionality for header

--- src/shared/components/Common/AppHeader/styles.ts
+++ src/shared/components/Common/AppHeader/styles.ts
@@ -2,6 +2,9 @@ import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
+  headerStickyContainer: {
+    zIndex: 5,
+  },
   header: {
     position: 'relative',
   },
--- src/shared/components/Common/AppHeader/types.ts
+++ src/shared/components/Common/AppHeader/types.ts
@@ -1,6 +1,10 @@
 import { WithStyles } from '@material-ui/core/styles/withStyles';
 import { styles } from './styles';
 
+export interface AppHeaderState {
+  showSearch: boolean;
+  stickyTriggerOffset: number;
+}
 export interface AppHeaderProps extends WithStyles<typeof styles> {
   isLoading: boolean;
   onMobileNavToggle(): void;
