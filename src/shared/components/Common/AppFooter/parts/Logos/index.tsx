c5e4ff69a0e1b6686d5a7f8827b326320cfb04e6
PavelKovalchuk <kovpavanat@gmail.com>
2018-11-19 18:20:22 +0200

    moved assests icons from footer

--- src/shared/components/Common/AppFooter/parts/Logos/index.tsx
+++ src/shared/components/Common/AppFooter/parts/Logos/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import { PartnerIconVisa } from '../../assets/partnerIconVisa';
-import { PartnerIconDhl } from '../../assets/partnerIconDhl';
-import { PartnerIconHermes } from '../../assets/partnerIconHermes';
-import { PartnerIconMasterCard } from '../../assets/partnerIconMasterCard';
-import { PartnerIconPaypal } from '../../assets/partnerIconPaypal';
+import { PartnerIconVisa } from '../../../../../assets/icons/partnerIconVisa';
+import { PartnerIconDhl } from '../../../../../assets/icons/partnerIconDhl';
+import { PartnerIconHermes } from '../../../../../assets/icons/partnerIconHermes';
+import { PartnerIconMasterCard } from '../../../../../assets/icons/partnerIconMasterCard';
+import { PartnerIconPaypal } from '../../../../../assets/icons/partnerIconPaypal';
 
 import { LogosProps as Props } from './types';
 import { styles } from './styles';
