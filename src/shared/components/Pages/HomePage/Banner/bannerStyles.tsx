b473e3871a26c0a4ca4d47bdaccf0aacc16dd680
PavelKovalchuk <kovpavanat@gmail.com>
2018-11-28 10:30:05 +0200

    banner for homepage

--- src/shared/components/Pages/HomePage/Banner/bannerStyles.tsx
+++ src/shared/components/Pages/HomePage/Banner/bannerStyles.tsx
@@ -3,5 +3,9 @@ import createStyles from '@material-ui/core/styles/createStyles';
 
 export const bannerStyles = (theme: Theme) => createStyles({
   root: {
+    // backgroundImage: `url("http://glue.de.suite.local:3000/img/homepage_hero_image.jpg")`,
+    backgroundColor: 'red',
+    backgroundSize: 'cover',
+    height: '700px',
   },
 });
