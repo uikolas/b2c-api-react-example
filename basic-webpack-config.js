95faf645148e8b58423f20255f3a60b31acf48d2
yevgenyermakov <een3838@gmail.com>
2018-11-29 17:54:30 +0200

    merge addresses

--- basic-webpack-config.js
+++ basic-webpack-config.js
@@ -164,13 +164,12 @@ const staticLoaders = [
   {test: /\.gif(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
   {test: /\.jpg|png$/, loader: "url-loader?limit=100000"},
   {
-    test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+    test: /.(ttf|otf|eot|svg|ico|woff(2)?)(\?[a-z0-9]+)?$/,
     use: [{
       loader: 'file-loader',
       options: {
         name: '[name].[ext]',
-        outputPath: 'fonts/', // where the fonts will go
-        publicPath: '../', // override the default path
+        publicPath: '/react/',
       },
     }],
   },
@@ -225,7 +224,7 @@ const webExtensions = [
   '.css', '.less', '.sass', '.scss',
   '.woff', '.woff2', '.ttf', '.eot',
   '.svg', '.md',
-  '.jpg', '.png',
+  '.jpg', '.png', '.ico',
 ];
 
 
