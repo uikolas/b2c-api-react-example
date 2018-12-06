2890111a4abb87b6636527823aac79f2e3819397
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-06 18:33:52 +0200

    fixed buttons size

--- src/shared/components/UI/SprykerButton/styles.tsx
+++ src/shared/components/UI/SprykerButton/styles.tsx
@@ -3,7 +3,7 @@ import createStyles from '@material-ui/core/styles/createStyles';
 
 export const styles = (theme: Theme) => createStyles({
   button: {
-    height: 45,
+    height: 44,
     borderRadius: theme.appFixedDimensions.borderRadius,
     fontSize: theme.appFixedDimensions.fontSize.medium,
     letterSpacing: 1.5,
