80466d41089cf1dd907a27528bae2a9737798ee4
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-14 17:48:35 +0200

    fixed GLUE-2776

--- src/shared/constants/content/homePage.ts
+++ src/shared/constants/content/homePage.ts
@@ -4,5 +4,5 @@ export const HomePageBannerIntro = 'Our newest laptop is packed full of the late
 export const HomePageBannerBtnText = 'Shop now';
 export const HomePageComputersBtnText = 'SEE ALL COMPUTERS';
 export const HomePageNotebooksBtnText = 'SEE ALL NOTEBOOKS';
-export const HomePageWorkstationsBtnText = 'SEE ALL TABLETS';
-export const HomePageTabletsBtnText = 'SEE ALL PCS & Workstations';
+export const HomePageWorkstationsBtnText = 'SEE ALL PCS & Workstations';
+export const HomePageTabletsBtnText = 'SEE ALL TABLETS';
