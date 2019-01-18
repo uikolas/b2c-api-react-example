f3a3e41fe0d7ddbfd100cb08e0ebf5347262502a
Aleksey Belan <alexey.belan@spryker.com>
2019-01-18 14:18:36 +0200

    Glue 3307 - changed code indents

--- src/shared/services/api/index.ts
+++ src/shared/services/api/index.ts
@@ -1,14 +1,14 @@
-import {create} from 'apisauce';
+import { create } from 'apisauce';
 import { config } from '../../config';
 import { TAccessToken } from '../../interfaces/login';
 
 const api = create({
-  baseURL: config.API_URL,
-  headers: {
-    'Accept': 'application/vnd.api+json',
-    'Content-Type': 'application/json',
-    'Accept-Language': 'en-EN,de;q=0.9,ru-UA;q=0.8,ru;q=0.7,uk-UA;q=0.6,uk;q=0.5,en-US;q=0.4,en;q=0.9',
-  },
+    baseURL: config.API_URL,
+    headers: {
+        'Accept': 'application/vnd.api+json',
+        'Content-Type': 'application/json',
+        'Accept-Language': 'en-EN,de;q=0.9,ru-UA;q=0.8,ru;q=0.7,uk-UA;q=0.6,uk;q=0.5,en-US;q=0.4,en;q=0.9',
+    },
 });
 
 export const nodeApi = create({baseURL: `http://${config.WEB_PATH}:${config.WEB_PATH}/nodeServer/`});
